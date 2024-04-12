f27b494faa3c21ca4f8d4b45574967ef672827d0
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-04-11 21:16:04 -0500

    Se añade la pregunta inicial en app.jss

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+'use strict';
+
+let user_name = prompt ('Bienvenido, soy Angel, ¿Cuál es tu nombre?');
+alert('Hola ' + user_name + ', vamos a ponerte a prueba para saber si logras adivinar algunas cosas de mí :D. Prepárate, jugaremos un juego muy bonito');
+// console.log('Hola ' + user_name + ', vamos a ponerte a prueba para saber si logras adivinar algunas cosas de mí :D. Prepárate, jugaremos un juego muy bonito');
+
